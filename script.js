934c2ff44730fe7deb8618fdef482efdda8f1cfa
unknown <amirtaha.main@gmal.com>
2024-05-29 12:33:56 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function getname () {
+    const person = {
+        firstname:"Amir Taha",
+        lastname:"Naeimi",
+        Age:"18",
+
+
+    }
+    console.log("My name is "+person.firstname+" "+person.lastname);
+
+}
+
+x=getname()
